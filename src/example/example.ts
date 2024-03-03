0259e0b1140f526a080beac7c01b6736a73e84ff
moritz <moritz@fluidkey.com>
2024-03-03 22:37:05 +0300

    add info in readme and adjust example

--- src/example/example.ts
+++ src/example/example.ts
@@ -74,17 +74,17 @@ export async function example({
     // the CREATE2 option with bytecode, making sure the addresses generated are the same
     console.log(`predicting Safe for signer ${stealthAddresses}`);
     const { stealthSafeAddress: stealthSafeAddressWithClient } = await predictStealthSafeAddressWithClient({
-      chainId,
       threshold: 1,
       stealthAddresses,
       safeVersion: '1.3.0',
+      useDefaultAddress: true,
     });
     const { stealthSafeAddress: stealthSafeAddressWithBytecode } = predictStealthSafeAddressWithBytecode({
-      chainId,
       threshold: 1,
       stealthAddresses,
       safeVersion: '1.3.0',
       safeProxyBytecode: '0x608060405234801561001057600080fd5b506040516101e63803806101e68339818101604052602081101561003357600080fd5b8101908080519060200190929190505050600073ffffffffffffffffffffffffffffffffffffffff168173ffffffffffffffffffffffffffffffffffffffff1614156100ca576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260228152602001806101c46022913960400191505060405180910390fd5b806000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505060ab806101196000396000f3fe608060405273ffffffffffffffffffffffffffffffffffffffff600054167fa619486e0000000000000000000000000000000000000000000000000000000060003514156050578060005260206000f35b3660008037600080366000845af43d6000803e60008114156070573d6000fd5b3d6000f3fea2646970667358221220d1429297349653a4918076d650332de1a1068c5f3e07c5c82360c277770b955264736f6c63430007060033496e76616c69642073696e676c65746f6e20616464726573732070726f7669646564',
+      useDefaultAddress: true,
     });
 
     console.log('  - stealthSafeAddressWithClient  ', stealthSafeAddressWithClient);
@@ -106,7 +106,7 @@ async function runExample() {
 WARNING: Only sign this message within a trusted website or platform to avoid loss of funds.
 
 Secret: deccc7b0ba824d3b6f73c50c41935eabf5e7e10f5b0177732344899c60be0f16`,
-    chainId: 11155111,
+    chainId: 0,
     startNonce: BigInt(0),
     endNonce: BigInt(30),
   });
